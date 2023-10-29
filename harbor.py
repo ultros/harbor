53d85bf4258a1c189c8c2240f045f9f7baa4ec18
Jesse Shelley <realjesseshelley@gmail.com>
2023-10-28 23:14:52 -0600

    modified pathing for the db

--- harbor.py
+++ harbor.py
@@ -23,8 +23,8 @@ def main():
 
     args = parser.parse_args()
 
-
-    db_path = f'{(pathlib.Path(os.readlink(__file__))).parent}/databases/harbor.db'
+    #db_path = f"{pathlib.Path(os.readlink(__file__)).parent}/databases/harbor.db"
+    db_path = f"{pathlib.Path().absolute()}/databases/harbor.db"
     print(db_path)
 
     dbu = DatabaseUtilities(db_path)  # create database if it does not exist
